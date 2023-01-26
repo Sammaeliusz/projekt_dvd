914d365554ac423d20f6759d142a6d6cf0967930
Sammaeliusz <93608787+Sammaeliusz@users.noreply.github.com>
2023-01-26 15:54:18 +0100

    new file:   back/README.txt 	new file:   back/base_file.py 	renamed:    modules.py -> back/modules.py 	renamed:    modules_error_code.txt -> back/modules_error_code.txt 	renamed:    modules_manual.txt -> back/modules_manual.txt 	new file:   front/index.php 	new file:   front/konto.php 	new file:   front/user_icon.png 	new file:   front/user_icon2.png 	new file:   front/user_icon_hover.png

---
+++ back/base_file.py
@@ -0,0 +1,32 @@
+import flask
+from abc import ABC, abstractmethod
+from modules.modules import *
+
+class App(flask.Flask):
+    route_list = []
+    def addroute(self, route:callable):
+        self.route_list.append(route)
+
+class Create(ABC):
+    class flask(ABC):
+        @abstractmethod
+        def route(app:flask.Flask, route:str, base_function:callable, methods=['GET'], *args, **kwargs) -> callable:
+            @app.route(route, methods=methods)
+            def f(*args, **kwargs):
+                return base_function(*args, **kwargs)
+            return f
+        
+        @abstractmethod
+        def dynamicRoute(app:flask.Flask, route:str, base_function:callable, methods=['GET'], *args, **kwargs) -> callable:
+            @app.route(route, methods=methods)
+            def f(*args, **kwargs):
+                return base_function(*args, **kwargs)
+            return f
+    class mainFunction(ABC):
+        @abstractmethod
+        def fromTemplate(template:str) -> callable:
+            return lambda : flask.Response(flask.stream_template(template))
+
+app = App(__name__, template_folder='material/projekt', static_folder='material/projekt')
+
+app.addroute(Create.flask.route(app, '/', Create.mainFunction.fromTemplate("index.html")))
