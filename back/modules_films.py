bc3e7c88fdf77500ee0e35e6c57c6897d090fa03
Sammaeliusz <kompyterowiec@gmail.com>
2023-02-02 11:07:28 +0100

    modified:   back/modules_films.py

--- back/modules_films.py
+++ back/modules_films.py
@@ -2,4 +2,6 @@ import mysql.connector as sql
 from mysql.connector.connection import MySQLConnection as mysql
 from mysql.connector import errorcode
 import re
-import modules_user.py
+import modules_user as mu
+
+conn = mu.connection()
