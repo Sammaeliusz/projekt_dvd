c3bf6b2aeeacc948e0f63e0bcb0361887227adc4
Sammaeliusz <93608787+Sammaeliusz@users.noreply.github.com>
2023-02-07 16:38:44 +0100

    modified:   back/modules_films.py

--- back/modules_films.py
+++ back/modules_films.py
@@ -6,4 +6,10 @@ import modules_user as mu
 def wyp_filmy(connection:sql.connection.MySQLConnection, user_id:int)->list:
     curs = connection.cursor()
     curs.execute("Select ")
+def ost_dod(connection:sql.connection.MySQLConnection, ilosc:int)->list:
+    curs = connection.cursor()
+    w = curs.execute(f"Select tytul from filmy order by id desc limit {ilosc}")
+    wynik = w.fetchall()
+    return wynik
 conn = mu.connection()
+ost_dod(conn, 5)
