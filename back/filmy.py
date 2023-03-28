19b3c33b6548276eadac25302aea2520ab095c52
MPPL <43617539+MPPL@users.noreply.github.com>
2023-03-28 05:28:01 +0200

    Delete back directory

--- back/filmy.py
+++
@@ -1,5 +0,0 @@
-from modules import *
-add_film(conn, "Avatar Istota Wody", "Sci-Fi", "James Cameron", 2022, 13)
-add_film(conn, "Black Panther 2018", "Akcja, Sci-Fi", " Ryan Coogler", 2018, 13)
-add_film(conn, "Kung Fury", "Parodia, Akcja, Sci-Fi, Komedia", "David Sandberg", 2015, 18)
-add_film(conn, "Finding Jesus", "Animacja, Rodzinny, Fantasy", "Jason Wright", 2020, 0)
