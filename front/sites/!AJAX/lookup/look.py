28e8b95e62feba13fb3065fd3f5d8004d3eae02f
MPPL <43617539+MPPL@users.noreply.github.com>
2023-05-25 01:39:58 +0200

    Add files via upload

---
+++ front/sites/!AJAX/lookup/look.py
@@ -0,0 +1,46 @@
+import sys
+from os import getcwd
+sys.path.append('../front/scripts')
+from utils import *
+
+def wrapper(function:callable, sql:SQL, **kwg) -> callable:
+    data = kwg['data']
+    title =             bottle.request.forms.get('title', '')
+    age_min =           bottle.request.forms.get('age_min', 0)
+    age_max =           bottle.request.forms.get('age_max', 99)
+    director =          bottle.request.forms.get('director', '')
+    production_min =    bottle.request.forms.get('production_min', 0)
+    production_max =    bottle.request.forms.get('production_max', 9999)
+    stock =             bottle.request.forms.get('stock', '0')
+    categories =        bottle.request.forms.get('categories', '')
+
+    x = lambda n, m : m if len(n) == 0 else int(n)
+
+    age_min = x(age_min, 0)
+    age_max = x(age_max, 99)
+    production_min = x(production_min, 0)
+    production_max = x(production_max, 9999)
+
+    movies = sql.movies_find_strict(tags=categories.split(','), name = title, age_min = int(age_min), age_max = int(age_max), director = director, production_min = int(production_min), production_max = int(production_max), onstock = stock=='1')
+    if movies.isUsefull():
+        y = movies.getList()
+        if isinstance(y[0], list):
+            ans = [Struct({
+                "id":x[0],
+                "name":x[1],
+                "categories":x[2],
+                "director":x[4],
+                "production":x[5],
+                "file":f"static/Filmy/{x[1].replace(' ', '-').replace(':','')}.png"
+            }) for x in y]
+        else:
+            ans = [Struct({
+                "id":y[0],
+                "name":y[1],
+                "categories":y[2],
+                "director":y[4],
+                "production":y[5],
+                "file":f"static/Filmy/{y[1].replace(' ', '-').replace(':','')}.png"
+            })]
+        return function(movies = ans)
+    return function(movies = [])
--- front/sites/film/film.py
+++ front/sites/film/film.py
@@ -4,12 +4,54 @@ sys.path.append('../front/scripts')
 from utils import *
 
 def wrapper(function:callable, sql:SQL, **kwg) -> callable:
+
     movie_id = int(bottle.request.query['mid'])
     question = sql.movie_finder(movie_id)
+    rent_error = 0
+    not_rented = True
+
+    if bottle.request.method == 'POST':
+        if bottle.request.forms.get('rent', None) == str(movie_id):
+            
+            rent_error = 1
+            user_id = bottle.request.get_cookie("id")
+            if user_id:
+                user_id = int(user_id)
+
+                user = sql.user_finder(user_id)
+
+                if user.isUsefull():
+                    q = sql.rent(movie_id, user_id, today(), today_add(days=14))
+
+                    if q.isUsefull():
+                        rent_error = 2
+
+
     if not question.isUsefull():
         return redirect('/')
+
     tags = sql.movie_tags(movie_id)
+
     if tags.isError() or tags.isInfo():
         sql.__log__(-10001, notes = f'{tags.getMessage()}')
         return function(movie=question.getList(), tags=[])
-    return function(movie=question.getList(), tags=tags.getList())
+    
+    user_id = bottle.request.get_cookie("id")
+    
+    if user_id:
+        user_id = int(user_id)
+
+        user = sql.user_finder(user_id)
+
+        if user.isUsefull():
+            q = sql.user_actual_rent(user_id)
+            if q.isUsefull():
+                q = q.getList()
+                if not isinstance(q[0],list):
+                    q = [q]
+                for x in q:
+                    if x[1] == movie_id:
+                        not_rented = False
+                        break
+
+    return function(movie=question.getList(), tags=tags.getList(), re = rent_error, not_rented = not_rented)
